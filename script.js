5a29c1733a9c1a00a47754cc705ef42c613148fd
schunsie <185822585+schunsie@users.noreply.github.com>
2025-01-02 23:04:46 +0100

    Add command line functionality of game
    - Added factory for user objects
    - Added game IIFE factory
    - Added basic function to manipulate gameBoard array

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-alert('test');
+const game = (function () {
+    const gameBoard = [
+        null, null, null,
+        null, null, null,
+        null, null, null
+    ]
+    
+    function takeTurn(gridNumber) {
+        gameBoard[gridNumber] = turn.getSymbol();
+        swapTurn()
+    }
+    
+    function swapTurn() {
+        turn = Player1 ? Player2 : Player1;
+    }
+    
+    function retrieveBoard() {
+        return gameBoard;
+    }
+    
+    function createPlayer(name, symbol) {
+        const getName = () => name;
+        const getSymbol = () => symbol;
+    
+        return { getName, getSymbol };
+    } 
+
+    let Player1 = createPlayer('Test One', 'O');
+    let Player2 = createPlayer('Test Two', 'X')
+    let turn = Player1;
+
+    return { takeTurn, retrieveBoard }
+})();

51f3c3a3fd4fe0d0379273e35eb086a489233a5b
schunsie <185822585+schunsie@users.noreply.github.com>
2025-01-06 11:54:47 +0100

    Adapt board array to be 2D and add start win check
    - Made the gameboard a 2d array
    - Put gameboard IIFE into game IIFE to keep functions private
    - Started on function to check for win conditions

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const game = (function () {
-    const gameBoard = [
-        null, null, null,
-        null, null, null,
-        null, null, null
-    ]
+    const gameBoard = (function () {
+        const gameBoard = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        
+        function placeSymbol(gridRow, gridIndex, symbol) {
+            if (gameBoard[gridRow][gridIndex] !== null) return false;
+            gameBoard[gridRow][gridIndex] = symbol;
+            return true;
+        }
     
-    function takeTurn(gridNumber) {
-        gameBoard[gridNumber] = turn.getSymbol();
-        swapTurn()
-    }
+        function retrieveBoard() {
+            return gameBoard;
+        }
     
-    function swapTurn() {
-        turn = Player1 ? Player2 : Player1;
-    }
-    
-    function retrieveBoard() {
-        return gameBoard;
-    }
-    
+        return { placeSymbol, retrieveBoard }
+    })();
+
     function createPlayer(name, symbol) {
         const getName = () => name;
         const getSymbol = () => symbol;
@@ -25,9 +26,45 @@ const game = (function () {
         return { getName, getSymbol };
     } 
 
-    let Player1 = createPlayer('Test One', 'O');
-    let Player2 = createPlayer('Test Two', 'X')
+    const Player1 = createPlayer('Test1', 'X');
+    const Player2 = createPlayer('Test2', 'O');
     let turn = Player1;
+    let turnCount = 0;
 
-    return { takeTurn, retrieveBoard }
+    function takeTurn(gridRow, gridIndex) {
+        if (gameBoard.placeSymbol(gridRow, gridIndex, turn.getSymbol())) {
+            turnCount++;
+            checkEndConditions() ? console.log(`${turn.getName()} has won!`) : swapTurn();
+            console.log(gameBoard.retrieveBoard());
+        }
+    }
+
+    function swapTurn() {
+        turn = turn === Player1 ? Player2 : Player1; 
+    }
+
+    function checkEndConditions() {
+        if (turnCount <= 9 && checkWinCondition()) return true;
+        // else if (turnCount === 9) return 'tie';
+        else return false;
+
+        function checkWinCondition() {
+            const board = gameBoard.retrieveBoard();
+            
+            // TODO: check win conditions
+            if (checkRowWin()) return true;
+            else return false;
+
+            function checkRowWin() {
+                for (const row of board) {
+                    if (row.every(item => item === turn.getSymbol())) return true;
+                    console.log(`No win found in ${row}`);
+                }
+                return false;
+            }
+
+        }
+    }
+
+    return { takeTurn }
 })();
